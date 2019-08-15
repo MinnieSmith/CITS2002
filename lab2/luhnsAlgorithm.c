45af44dc7a3d542bea3b061125c29b5e357292e0
Minnie Smith <jacta-alea-est@hotmail.com>
2019-08-15 17:51:46 +0800

    Luhn

--- lab2/luhnsAlgorithm.c
+++ lab2/luhnsAlgorithm.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int convert(char c)
+{
+    return(c - '0');
+}
 
 int main (int argcount, char *argvalue[]) {
-    char ccNumber[17] = argvalue[1];
-    int ccLength = strlen(ccNumber);
-    printf("%d\n", ccLength);
+    int s1 = 0;
+    int s2 = 0;
+    int i;
+    int j;
+    int length = strlen(argvalue[1]);
+
+    if(length != 16) {
+        printf("Please enter a valid 16 digit credit card number");
+    } else {
+        for(i = length-1; i >= 0; i = i-2){
+            s1 = s1 + convert(argvalue[1][i]);
+        }
+        for (j = length-2; j>=0; j = j-2) {
+            s2 = s2 + (convert(argvalue[1][j])*2);
+        }        
+
+        printf("%d\n", s1+s2);
+        if(s1+s2 % 10 == 0) {
+        printf("This is a valid credit card number");
+        } else {
+        printf("This is not a valid credit card number");
+        }
+    }
+    
+    return 0;
 }
