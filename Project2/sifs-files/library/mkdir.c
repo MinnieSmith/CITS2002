e2407d00ac512e6ff45c4d3ac11cfab9010a1b49
Minnie Smith <jacta-alea-est@hotmail.com>
2019-10-08 23:15:48 +0800

    Mkdir done!

--- Project2/sifs-files/library/mkdir.c
+++ Project2/sifs-files/library/mkdir.c
@@ -25,7 +25,6 @@ int SIFS_mkdir(const char *volumename, const char *dirname)
         int nblocks;
         int blocksize;
 
-
         // ATTEMPT TO OPEN THE VOLUME FOR READ-ONLY ACCESS
         FILE *fp = fopen(volumename, "r+");
 
@@ -36,61 +35,84 @@ int SIFS_mkdir(const char *volumename, const char *dirname)
             return 1;
         }
 
-
-
         // FIND THE NUMBER OF BLOCKS
         fread(&hd, sizeof(SIFS_VOLUME_HEADER), 1, fp);
         nblocks = hd.nblocks;
         blocksize = hd.blocksize;
         printf("%i, %zu \n", nblocks, hd.blocksize);
-        
 
         // SIFS_DIRBLOCK root;
         SIFS_BIT btmp[nblocks];
+        SIFS_DIRBLOCK dirblocks;
         int free_block_index = 0;
-       
+
         // FIND THE INDEX OF THE FIRST FREE BLOCK
         fread(&btmp, sizeof(btmp), 1, fp);
-        // printf("Bitmap: %s\n", btmp);
+        printf("Bitmap: %s\n", btmp);
         // printf("Number of blocks %i\n", nblocks);
-        
-        for (int b=1; b<(nblocks-1); ++b)
+
+
+        for (int b = 1; b < (nblocks - 1); ++b)
+        {
+            // printf("Inloop: %c, %i\n", btmp[b], b);
+
+            if (btmp[b] == SIFS_UNUSED)
             {
-                // printf("Inloop: %c, %i\n", btmp[b], b);
-                
-                if(btmp[b] == SIFS_UNUSED)
+                free_block_index = b;
+                break;
+            }
+        }
+        printf("free block index %i\n", free_block_index);
+
+        // FIND NUMBER OF DIRECTORIES IN VOLUME
+        int ndir = 0;
+        for (int i =0; i <nblocks-1; ++i )
+        {
+            if(btmp[i] == SIFS_DIR)
+            {
+                ndir++;
+            }
+        }
+        printf("number of directories in volume: %i\n", ndir);
+
+
+        // FIND THE BLOCK NUMBER OF ALL THE DIRECTORIES IN FILE VIA BITMAP
+        int dir_block_number[ndir-1];
+
+        int n = 0;
+
+        {
+            for (int b = 1; b < ndir; ++b)
+            {
+                if (btmp[b] == SIFS_DIR)
                 {
-                    printf("%c\n", btmp[b]);
-                    free_block_index = b;
-                    break;
+                    dir_block_number[n] = b;
+                    n++;
+
                 }
             }
-            printf("%i\n", free_block_index);        
-        
-        // FIND THE BLOCK NUMBER OF ALL THE DIRECTORIES IN FILE
-        int dir_block_number [nblocks];
+        }
 
-        for (int b=1; b<(nblocks-1); ++b)
+        // IF DIRECTORY ALREADY EXISTS
+        for (int i = 0; i < ndir-1; ++i)
+        {
+            int dir_location = sizeof(hd) + sizeof(btmp) + blocksize + (blocksize*(i+1));
+            fseek(fp, dir_location, SEEK_SET);
+            fread(&dirblocks, sizeof(dirblocks), 1, fp);
+            if(strcmp(dirblocks.name, dirname) == 0)
             {
-                int i = 0;
-                if(btmp[b] == SIFS_DIR)
-                {
-                    dir_block_number[i] = b;
-                    i++;
-                }
+                SIFS_errno = SIFS_EEXIST;
+                return 1;
             }
-        
-        // TODO: CHECK TO SEE IF DIRECTORY NAME ALREADY EXISTS
-        // fread(&db, sizeof(SIFS_DIRBLOCK), 1, fp);
-        // if (strcmp(db.name, dirname) == 0)
-        //     SIFS_errno = SIFS_EEXIST;
-
+            
+        }
 
 
         //  UPDATE BITMAP
         btmp[free_block_index] = SIFS_DIR;
-        fseek(fp, sizeof(SIFS_VOLUME_HEADER), SEEK_SET);
+        fseek(fp, sizeof(hd), SEEK_SET);
         fwrite(btmp, sizeof(btmp), 1, fp);
+
 
         //  CREATE DIRECTORY
         SIFS_DIRBLOCK newdir;
@@ -102,24 +124,17 @@ int SIFS_mkdir(const char *volumename, const char *dirname)
         newdir.nentries = 0;
 
         int newdir_location = 0;
-        newdir_location = sizeof(SIFS_VOLUME_HEADER) + sizeof(btmp) + (free_block_index * sizeof(oneblock));
+        newdir_location = sizeof(hd) + sizeof(btmp) + (free_block_index * sizeof(oneblock));
 
         fseek(fp, newdir_location, SEEK_SET);
         fwrite(&newdir, sizeof(oneblock), 1, fp);
 
-    
-
-
         fclose(fp);
         return 0;
-
-
-
     }
     else
     {
         SIFS_errno = SIFS_ENOENT;
         return 1;
     }
-    
 }
