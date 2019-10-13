9fcb06d245af17c466610004833a605ecd54010f
Minnie Smith <jacta-alea-est@hotmail.com>
2019-10-13 18:10:31 +0800

    Writefile testing: no space left on volume?

--- Project2/sifs-files/library/writefile.c
+++ Project2/sifs-files/library/writefile.c
@@ -33,7 +33,7 @@ int SIFS_writefile(const char *volumename, const char *pathname,
             return 1;
         }
 
-        // FIND THE NUMBER OF BLOCKS
+        // FIND THE NUMBER OF BLOCKS IN VOLUME
         SIFS_VOLUME_HEADER hd;
         int nblocks;
         int blocksize;
@@ -50,78 +50,6 @@ int SIFS_writefile(const char *volumename, const char *pathname,
         fseek(fp, sizeof(hd), SEEK_SET);
         fread(&btmp, sizeof(btmp), 1, fp);
         printf("Bitmap: %s\n", btmp);
-
-        int free_block_count = 0;
-        for (int b = 1; b < nblocks; ++b)
-        {
-            // printf("Inloop: %c, %i\n", btmp[b], b);
-
-            if (btmp[b] == SIFS_UNUSED)
-            {
-                free_block_count++;
-            }
-        }
-
-        // CALCULATE THE NUMBER OF BLOCKS REQURIED FOR DATA
-        int data_blocks_required = ceil(nbytes / blocksize);
-        int total_blocks_required = data_blocks_required +1;
-
-        if(total_blocks_required>free_block_count)
-        {
-            SIFS_errno = SIFS_ENOSPC;
-            return 1;
-        }
-
-        // FIND INDEX OF FIRST FIRST BLOCK WITH ADEQUATE CONTIGUOUS BLOCKS
-        int unused_block_count = 0;
-        int free_contiguous_block_number = 0; // block number of where the contiguous block starts
-        bool enough_datablocks = false;
-        for (int b = 1; b < nblocks; ++b)
-        {
-            if (btmp[b] == SIFS_UNUSED)
-            {
-                unused_block_count++;
-                if (unused_block_count == data_blocks_required)
-                {
-                    free_contiguous_block_number = b;
-                    enough_datablocks = true;
-                }
-            }
-            else
-            {
-                unused_block_count = 0;
-            }
-        }
-
-        if (!enough_datablocks)
-        {
-            SIFS_errno = SIFS_ENOSPC;
-        }
-
-        // TODO: WRITE DATA TO DATABLOCKS
-
-        // FIND INDEX OF FREE BLOCK FOR FILEBLOCK ASSIGNMENT
-        int free_block_index = 0;
-        for (int b = 1; b < nblocks; ++b)
-        {
-            // printf("Inloop: %c, %i\n", btmp[b], b);
-
-            if (btmp[b] == SIFS_UNUSED)
-            {
-                free_block_index = b;
-                break;
-            }
-        }
-
-        // CHECK TO SEE IF THERE ARE ANY FREE BLOCKS
-        if (free_block_index == 0)
-        {
-            SIFS_errno = SIFS_ENOSPC;
-            return 1;
-        }
-        printf("free block index %i\n", free_block_index);
-
-        // TODO: ASSIGN FILE BLOCK
 
         // REMOVE ANY '/' FROM PATHNAME
         char *path_tokens[nblocks * SIFS_MAX_NAME_LENGTH];
@@ -269,71 +197,216 @@ int SIFS_writefile(const char *volumename, const char *pathname,
             }
         }
 
-        // CHECK IF DIRECTORY NAME ALREADY EXISTS
-        for (int i = 0; i < ndir; i++)
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        // CHECK FOR DUPLICATE FILES IN VOLUME
+        //----------------------------------------------------------------------------------------------------------------------------------------
+
+        // CALCULATE THE MD5 OF THE DATA
+        unsigned char md5_infile[MD5_BYTELEN];
+        MD5_buffer(data, nbytes, md5_infile);
+        printf("md5 %s\n", MD5_format(md5_infile));
+        bool duplicate_md5 = false;
+
+        int block_number_of_duplicate_md5 = 0;
+
+        //  CHECK IF MD5 ALREADY EXISTS
+        for (int i = 0; i < nfile; ++i)
         {
-            int dir_location = sizeof(hd) + sizeof(btmp) + (blocksize * dir_block_number[i]);
-            fseek(fp, dir_location, SEEK_SET);
-            fread(&dirblocks, sizeof(dirblocks), 1, fp);
-            printf("dirblock name: %s\n", dirblocks.name);
-            if (strcmp(dirblocks.name, path_tokens[t - 1]) == 0)
+            int file_location = sizeof(hd) + sizeof(btmp) + (blocksize * file_block_number[i]);
+            fseek(fp, file_location, SEEK_SET);
+            fread(&fileblock, sizeof(fileblock), 1, fp);
+            char md5_str[MD5_STRLEN];
+            memcpy(md5_str, md5_infile, sizeof(md5_infile));
+
+            if (memcmp(md5_str, fileblock.md5, sizeof(md5_str)) == 0)
             {
-                SIFS_errno = SIFS_EEXIST;
-                return 1;
+                strcpy(fileblock.filenames[fileblock.nfiles], path_tokens[t - 1]);
+                fileblock.nfiles++;
+                duplicate_md5 = true;
+                block_number_of_duplicate_md5 = i;
+                fseek(fp, file_location, SEEK_SET);
+                fwrite(&fileblock, sizeof(fileblock), 1, fp);
+                printf("Duplicate found, fileblock updated\n");
             }
         }
 
-        //  UPDATE BITMAP
-        btmp[free_block_index] = SIFS_DIR;
-        fseek(fp, sizeof(hd), SEEK_SET);
-        fwrite(btmp, sizeof(btmp), 1, fp);
-
-        //  CREATE DIRECTORY
-        SIFS_DIRBLOCK newdir;
-        char oneblock[blocksize];
-
-        // UPDATE VOLUME WITH NEW DIRECTORY
-        strcpy(newdir.name, path_tokens[t - 1]);
-        newdir.modtime = time(NULL);
-        newdir.nentries = 0;
-
-        int newdir_location = 0;
-        newdir_location = sizeof(hd) + sizeof(btmp) + (free_block_index * blocksize);
-
-        fseek(fp, newdir_location, SEEK_SET);
-        fwrite(&newdir, sizeof(oneblock), 1, fp);
-
-        // UPDATE THE ROOT
-        if (t == 1)
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        // IF MDU5 EXISTS
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        if (duplicate_md5)
         {
-            int root_location = sizeof(hd) + sizeof(btmp);
-            fseek(fp, root_location, SEEK_SET);
-            fread(&dirblocks, sizeof(dirblocks), 1, fp);
-            dirblocks.entries[dirblocks.nentries].blockID = free_block_index;
-            dirblocks.nentries += 1;
-            fseek(fp, root_location, SEEK_SET);
-            fwrite(&dirblocks, sizeof(dirblocks), 1, fp);
+            // UPDATE THE ROOT
+            if (t == 1)
+            {
+                int root_location = sizeof(hd) + sizeof(btmp);
+                fseek(fp, root_location, SEEK_SET);
+                fread(&dirblocks, sizeof(dirblocks), 1, fp);
+                if (dirblocks.nentries == SIFS_MAX_ENTRIES)
+                {
+                    SIFS_errno = SIFS_EMAXENTRY;
+                    return 1;
+                }
+                else
+                {
+                    dirblocks.entries[dirblocks.nentries].blockID = block_number_of_duplicate_md5;
+                    dirblocks.entries[dirblocks.nentries].fileindex = fileblock.nfiles - 1;
+                    dirblocks.nentries += 1;
+                    fseek(fp, root_location, SEEK_SET);
+                    fwrite(&dirblocks, sizeof(dirblocks), 1, fp);
+                }
+            }
+
+            // UPDATE ENTRY ON SUBDIRECTORY
+            if (t > 1)
+            {
+                SIFS_DIRBLOCK subdir_to_be_updated;
+                int subdir_to_be_updated_location = sizeof(hd) + sizeof(btmp) + (blocksize * subdir_block_id[s - 1]);
+                fseek(fp, subdir_to_be_updated_location, SEEK_SET);
+                fread(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+
+                if (subdir_to_be_updated.nentries == SIFS_MAX_ENTRIES)
+                {
+                    SIFS_errno = SIFS_EMAXENTRY;
+                    return 1;
+                }
+                else
+                {
+                    subdir_to_be_updated.entries[subdir_to_be_updated.nentries].blockID = block_number_of_duplicate_md5;
+                    subdir_to_be_updated.entries[subdir_to_be_updated.nentries].fileindex = fileblock.nfiles - 1;
+                    subdir_to_be_updated.nentries += 1; // update number of entries
+                    fseek(fp, subdir_to_be_updated_location, SEEK_SET);
+                    fwrite(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+                    return 0;
+                }
+            }
         }
 
-        // UPDATE ENTRY ON SUBDIRECTORY
-        if (t > 1)
-        {
-            SIFS_DIRBLOCK subdir_to_be_updated;
-            int subdir_to_be_updated_location = sizeof(hd) + sizeof(btmp) + (blocksize * subdir_block_id[s - 1]);
-            fseek(fp, subdir_to_be_updated_location, SEEK_SET);
-            fread(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+        //----------------------------------------------------------------------------------------------------------------------------------------
+        // IF THERE ARE NO DUPLICATE FILES ALREADY IN VOLUME
+        //----------------------------------------------------------------------------------------------------------------------------------------
 
-            if (subdir_to_be_updated.nentries == SIFS_MAX_ENTRIES)
+        if (!duplicate_md5)
+        {
+            // CALCULATE THE NUMBER OF BLOCKS REQURIED FOR DATA
+            int data_blocks_required = ceil(nbytes / blocksize);
+            int total_blocks_required = data_blocks_required + 1;
+            int free_block_count = 0;
+            for (int b = 1; b < nblocks; ++b)
             {
-                SIFS_errno = SIFS_EMAXENTRY;
+                if (btmp[b] == SIFS_UNUSED)
+                {
+                    free_block_count++;
+                }
+            }
+
+            if (total_blocks_required > free_block_count)
+            {
+                SIFS_errno = SIFS_ENOSPC;
                 return 1;
             }
 
-            subdir_to_be_updated.entries[subdir_to_be_updated.nentries].blockID = free_block_index;
-            subdir_to_be_updated.nentries += 1; // update number of entries
-            fseek(fp, subdir_to_be_updated_location, SEEK_SET);
-            fwrite(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+            // FIND INDEX OF FIRST FIRST BLOCK WITH ADEQUATE CONTIGUOUS BLOCKS
+            int unused_block_count = 0;
+            int free_contiguous_block_number = 0; // block number of where the contiguous block starts
+            bool enough_datablocks = false;
+            for (int b = 1; b < nblocks; ++b)
+            {
+                if (btmp[b] == SIFS_UNUSED)
+                {
+                    unused_block_count++;
+                    if (unused_block_count == data_blocks_required)
+                    {
+                        free_contiguous_block_number = b - (data_blocks_required - 1);
+                        enough_datablocks = true;
+                    }
+                }
+                else
+                {
+                    unused_block_count = 0;
+                }
+            }
+
+            if (!enough_datablocks)
+            {
+                SIFS_errno = SIFS_ENOSPC;
+                return 1;
+            }
+
+            // WRITE DATA TO DATABLOCKS
+            int data_block_location = sizeof(hd) + sizeof(btmp) + (blocksize * free_contiguous_block_number);
+            fseek(fp, data_block_location, SEEK_SET);
+            for (int i = 0; i < data_blocks_required; ++i)
+            {
+                fwrite((char*)data + (i * blocksize), blocksize, 1, fp);
+                printf("data written to datablock\n");
+                btmp[free_contiguous_block_number + i] = SIFS_FILE;
+            }
+
+            // UPDATE BITMAP
+            fseek(fp, sizeof(hd), SEEK_SET);
+            fwrite(btmp, sizeof(btmp), 1, fp);
+
+            // FIND INDEX OF FREE BLOCK FOR FILEBLOCK ASSIGNMENT
+            int free_block_index = 0;
+            for (int b = 1; b < nblocks; ++b)
+            {
+
+                if (btmp[b] == SIFS_UNUSED)
+                {
+                    free_block_index = b;
+                    break;
+                }
+            }
+
+            // CREATE FILEBLOCK AND UPDATE VOLUME NEW FILEBLOCK
+            SIFS_FILEBLOCK newfile;
+            strcpy(newfile.filenames[0], path_tokens[t - 1]);
+            newfile.firstblockID = free_contiguous_block_number;
+            char *md5_formatted = MD5_format(md5_infile);
+            memcpy(newfile.md5, md5_formatted,sizeof(*md5_formatted));
+            newfile.length = nbytes;
+            newfile.nfiles = 1;
+            newfile.modtime = time(NULL);
+
+            int newfile_location = sizeof(hd) + sizeof(btmp) + (free_block_index * blocksize);
+            fseek(fp, newfile_location, SEEK_SET);
+            fwrite(&newfile, blocksize, 1, fp);
+
+            // UPDATE THE ROOT
+            if (t == 1)
+            {
+                int root_location = sizeof(hd) + sizeof(btmp);
+                fseek(fp, root_location, SEEK_SET);
+                fread(&dirblocks, sizeof(dirblocks), 1, fp);
+                dirblocks.entries[dirblocks.nentries].blockID = free_block_index;
+                dirblocks.entries[dirblocks.nentries].fileindex = 0;
+                dirblocks.nentries += 1;
+                fseek(fp, root_location, SEEK_SET);
+                fwrite(&dirblocks, sizeof(dirblocks), 1, fp);
+            }
+
+            // UPDATE ENTRY ON SUBDIRECTORY
+            if (t > 1)
+            {
+                SIFS_DIRBLOCK subdir_to_be_updated;
+                int subdir_to_be_updated_location = sizeof(hd) + sizeof(btmp) + (blocksize * subdir_block_id[s - 1]);
+                fseek(fp, subdir_to_be_updated_location, SEEK_SET);
+                fread(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+
+                if (subdir_to_be_updated.nentries == SIFS_MAX_ENTRIES)
+                {
+                    SIFS_errno = SIFS_EMAXENTRY;
+                    return 1;
+                }
+
+                subdir_to_be_updated.entries[subdir_to_be_updated.nentries].blockID = free_block_index;
+                subdir_to_be_updated.entries[subdir_to_be_updated.nentries].fileindex = 0;
+                subdir_to_be_updated.nentries += 1; // update number of entries
+                fseek(fp, subdir_to_be_updated_location, SEEK_SET);
+                fwrite(&subdir_to_be_updated, sizeof(subdir_to_be_updated), 1, fp);
+            }
         }
+
         fclose(fp);
         return 0;
     }
@@ -342,3 +415,5 @@ int SIFS_writefile(const char *volumename, const char *pathname,
         SIFS_errno = SIFS_ENOENT;
         return 1;
     }
+}
+
